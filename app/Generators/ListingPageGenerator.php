b656517c94c356b0f179f1d3aff4305458efce54
Bill Mitchell <wkmitch@gmail.com>
2016-03-17 01:35:29 +0000

    Finishing up

--- app/Generators/ListingPageGenerator.php
+++ app/Generators/ListingPageGenerator.php
@@ -9,20 +9,21 @@ class ListingPageGenerator
     protected $resultData = [];
 
 
-    public function generateHtml($listingsJson)
+    public function formatData($listingsJson)
     {
         $listingArray = json_decode($listingsJson, true);
 
         foreach ($listingArray['Results'] as $listing) {
-            $newListing = [];
-            $newListing['price'] = $listing['Property']['Price'];
-            $newListing['description']     = $listing['PublicRemarks'];
-            $newListing['address'] = $listing['Property']['Address']['AddressText'];
-            $newListing['url'] = env('REALTOR_DOMAIN') . $listing['RelativeDetailsURL'];
 
+            $newListing = [];
+            $newListing['price']       = $listing['Property']['Price'];
+            $newListing['description'] = $listing['PublicRemarks'];
+            $newListing['address']     = $listing['Property']['Address']['AddressText'];
+            $newListing['url']         = env('REALTOR_DOMAIN') . $listing['RelativeDetailsURL'];
+            $newListing['image']       = $listing['Property']['Photo'][0]['MedResPath'];
+            $newListing['lastUpdated'] = $listing['Property']['Photo'][0]['LastUpdated'];
             $this->resultData[] = $newListing;
         }
-
-        return $this->resultData;
+        return ['listings' => $this->resultData];
     }
 }
