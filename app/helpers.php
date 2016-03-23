c2cfad73ac8140f7a014e18f3c74814a865176d6
Bill Mitchell <wkmitch@gmail.com>
2016-03-23 23:26:43 +0000

    Add placemarker to googlemap

--- app/helpers.php
+++ app/helpers.php
@@ -5,5 +5,5 @@ function getGoogleMapUrl($address)
 {
 	$key = env('GOOGLE_MAPS_API_KEY');
 	$address = str_replace('|', ' ', $address);
-	return  "https://maps.googleapis.com/maps/api/staticmap?center={$address}&key={$key}&size=200x200&zoom=14";
+	return  "https://maps.googleapis.com/maps/api/staticmap?center={$address}&markers=size:mid%7Ccolor:0xFFFF00%7C{$address}&key={$key}&size=256x200&zoom=14";
 }
